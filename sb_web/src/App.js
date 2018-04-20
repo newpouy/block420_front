d021535aa285991bc8ce4ffbfbcc48f94f34376d
newpouy <newpouy@gmail.com>
2018-04-21 01:35:41 +0900

    add Jumbotron

--- sb_web/src/App.js
+++ sb_web/src/App.js
@@ -4,16 +4,15 @@ import './App.css';
 import Artist from './Artist'
 import Listener from './Listener'
 
-import { Tab, Tabs } from 'react-bootstrap';
+import { Tab, Tabs,Jumbotron, Button} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <Jumbotron>
+        <h1>SoundBlocks</h1>
+        </Jumbotron>
         <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
           <Tab eventKey={1} title="Artist">
             <Artist/>
@@ -21,7 +20,7 @@ class App extends Component {
           <Tab eventKey={2} title="Listener">
             <Listener/>
           </Tab>
-        </Tabs>;
+        </Tabs>
       </div>
     );
   }
