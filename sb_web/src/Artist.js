b6cef18aac3c685215e1319fc373a897082a9e25
Jake <mubok0212@gmail.com>
2018-04-21 06:20:53 +0900

    import contract which is deployed on blockchain

--- sb_web/src/Artist.js
+++ sb_web/src/Artist.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, ButtonToolbar } from 'react-bootstrap';
 
-import { Tab } from 'react-bootstrap';
+import './Artist.css';
 
 class Artist extends Component {
+  
   render() {
     return (
       <div>
-        artist
+        <div id="upload-box" >
+          <input className="upload" type="file" />
+          <ButtonToolbar className="btn-wrapper">
+            <Button bsStyle="primary">업로드</Button>
+          </ButtonToolbar>
+        </div>
       </div>
-    );
+    )
   }
 }
 
