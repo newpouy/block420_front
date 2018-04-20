050523a4f8a8f87a8745d2b27425de984f105d3b
wooqii <cpawookie@gmail.com>
2018-04-21 02:23:52 +0900

    delete trash

--- contracts/Block420Token.sol
+++ contracts/Block420Token.sol
@@ -31,27 +31,8 @@ contract Block420Token {
         return balances[_owner];
     }
     
-    function transferFrom(address _from, address _to, uint256 _value) public returns(bool) {
-        require(_to != address(0));
-        require(_value <= balances[_from]);
-        require(_value <= allowed[_from][msg.sender]);
-        balances[_from] = balances[_from] - _value;
-        balances[_to] = balances[_to] + _value;
-        allowed[_from][msg.sender] = allowed[_from][msg.sender] - _value;
-        emit Transfer(_from, _to, _value);
-        return true;
-    }
-
-    function approve(address _spender, uint256 _value) public returns (bool) {
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) public view returns (uint256) {
-        return allowed[_owner][_spender];
-    }
-
+    
+    
     function mint(address _to, uint256 _amount) public returns (bool) {
         require(msg.sender == owner);
         totalSupply = totalSupply + _amount;
